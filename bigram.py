2c66d22523b418a4afddcaacf9d9b432828edc12
Li Wang <awaliwang@gmail.com>
2022-09-16 00:08:23 -0500

    added some preprocessing, bigram complete, need training function

--- bigram.py
+++ bigram.py
@@ -29,6 +29,27 @@ class node():
         self.total = 0
         branches = {}
 
+def preprocessing(training_set):
+    file = 'a1/A1/A1_DATASET/train/{training_set}.txt'.format(training_set = training_set)
+    processed_file = 'a1/A1/A1_DATASET/train/{training_set}_processed.txt'.format(training_set = training_set)
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    lines = ['<s> ' + line + '<stop>' for line in lines]
+    with open(processed_file, 'w') as f:
+        f.writelines(lines)
 
+    return
 
-think = "I booked two rooms four months in advance at the Talbott . We were placed on the top floor next to the elevators , which are used all night long . When speaking to the front desk , I was told that they were simply honoring my request for an upper floor , which I had requested for a better view . I am looking at a brick wall , and getting no sleep . He also told me that they had received complaints before from guests on the 16th floor , and were aware of the noise problem . Why then did they place us on this floor when the hotel is not totally booked ? A request for an upper floor does not constitute placing someone on the TOP floor and using that request to justify this . If you decide to stay here , request a room on a lower floor and away from the elevator ! I spoke at length when booking my two rooms about my preferences . This is simply poor treatment of a guest whom they believed would not complain ."
+def train(bigram):
+    
+    bigram = Bigram()
+    with open('a1/A1/A1_DATASET/train/truthful.txt') as f:
+        for line in f.readlines():
+            words = line.split()
+
+    return
+
+preprocessing('deceptive')
+preprocessing('truthful')
+
+think = "<s> I booked two rooms four months in advance at the Talbott . We were placed on the top floor next to the elevators , which are used all night long . When speaking to the front desk , I was told that they were simply honoring my request for an upper floor , which I had requested for a better view . I am looking at a brick wall , and getting no sleep . He also told me that they had received complaints before from guests on the 16th floor , and were aware of the noise problem . Why then did they place us on this floor when the hotel is not totally booked ? A request for an upper floor does not constitute placing someone on the TOP floor and using that request to justify this . If you decide to stay here , request a room on a lower floor and away from the elevator ! I spoke at length when booking my two rooms about my preferences . This is simply poor treatment of a guest whom they believed would not complain . <stop>"
