191f8536d2d14c7ef76c46606d901e2e3a86af9f
user <bxl190001@utdallas.edu>
2022-09-22 12:25:53 -0500

    do not train on validation set Generalize n to arbitrary positive integer Move tests to after ngrams are trained

--- a1/ngram.py
+++ a1/ngram.py
@@ -18,85 +18,95 @@ class Use(IntEnum):
 
 class Ngram():
     def __init__(self):
-        self.root = {}
-        self.total = 0
-    
-    def insert(self, previous, current):
-        if previous not in self.root:
-            self.total += 1
-            self.root[previous] = Node()
-            self.root[previous].total += 1
+        self.root = Node()
+        self.total = 0 # vocabulary size
+
+    def insert(self, words):
+        cur = self.root
+
+        for word in words:
+            if word not in cur.branches:
+                self.total += 1
+                cur.branches[word] = Node()
+            self.root.total += 1 # total number of words
+            cur.branches[word].total += 1
+            cur = cur.branches[word]
+
+    def prob(self, words):
+        if len(words) <= 0:
+            return 0
+
+        cur = self.root
+
+        for word in words[:-1]:
+            if word not in cur.branches:
+                return 0
+            cur = cur.branches[word]
+
+        if words[-1] not in cur.branches:
+            return 0
+        return cur.branches[words[-1]].total / cur.total
+
+    def laplace(self, words):
+        return self.add_r(1, words)
+
+    def add_r(self, r, words):
+        if r <= 0 or self.root.total <= 0 or len(words) <= 0:
+            return -1
+
+        prob = r / (r * self.root.total)
+        cur = self.root
+
+        for word in words[:-1]:
+            if word not in cur.branches:
+                return prob
+            cur = cur.branches[word]
+
+        if words[-1] not in cur.branches:
+            count = 0
         else:
-            self.root[previous].total += 1
-        if current not in self.root[previous].branches:
-            self.root[previous].branches[current] = 1
-        else:
-            self.root[previous].branches[current] += 1
+            count = cur.branches[words[-1]].total
 
-    def prob(self, previous, next):
-        prob = 0
-        if previous not in self.root:
-            return prob
-        elif next not in self.root[previous].branches:
-            return prob
-        else:
-            prob = self.root[previous].branches[next] / self.root[previous].total
+        return (count + r) / (cur.total + r * self.total)
 
-        return prob
-
-    def laplace(self, previous, next):
-        return self.add_r(1, previous, next)
-
-    def add_r(self, r, previous, next):
-        if previous not in self.root:
-            previous = '<unknown>'
-        if next not in self.root[previous].branches:
-            next = '<unknown>'
-        prev_node = self.root[previous]
-        return (prev_node.branches[next] + r) / (prev_node.total + r * self.total)
 
 class Node():
     def __init__(self):
         self.total = 0
         self.branches = {}
 
-def train(ngram, file):
-    with open(file, 'r', errors='replace') as f:
-        lines = f.read().splitlines()
-
+def train(ngram, lines, N):
     for line in lines:
         words = line.split(' ')
-        for i in range(1, len(words)):
-            ngram.insert(words[i - 1], words[i])
+        for i in range(N, len(words) + 1):
+            ngram.insert(words[i - N:i])
 
-def perplexity(ngram, words):
-    for i in range(1, len(words)):
-        ngram.insert(words[i - 1], words[i])
-
+def perplexity(ngram, words, N, r):
     sum = 0
 
-    for i in range(1, len(words)):
-        sum -= log(ngram.prob(words[i - 1], words[i]))
+    for i in range(N, len(words) + 1):
+        prob = ngram.add_r(r, words[i - N:i])
+        if prob <= 0:
+            return -1
+        sum -= log(prob)
 
     return e ** (sum / len(words))
 
-def test(ngrams, testFile, labelFile):
-    with open(testFile, 'r', errors='replace') as f:
-        tests = f.read().splitlines()
-
-    with open(labelFile, 'r', errors='replace') as f:
-        labels = f.read().splitlines()
-
+def test(ngrams, tests, labels, N, r):
     sum = 0
     for i in range(len(tests)):
-        outputs = [perplexity(ngrams[c], tests[i]) for c in Class]
-        # print(f'{labels[i]} {outputs} {min(outputs)}')
+        test_words = tests[i].split(' ')
+        outputs = [perplexity(ngrams[c], test_words, N, r) for c in Class]
+        #print(f'{labels[i]} {outputs} {min(outputs)}')
         sum += (int(labels[i]) == outputs.index(min(outputs)))
 
-    print(sum / len(tests))
+    print(f'Length: {N}, Accuracy: {sum / len(tests)}')
+    print(f'Correct: {sum}')
      
 
 START_INDEX = 1
+
+N = 2 # unigram, bigram
 
 def main():
     if len(argv) < START_INDEX + len(Class):
@@ -105,7 +115,8 @@ def main():
             exit(1)
 
     files = [None] * len(Use)
-    ngrams = [Ngram() for c in Class]
+    ngrams = [[Ngram() for c in Class] for l in range(N)]
+    sets = [None] * len(Use)
 
     for c in Class:
         path = argv[START_INDEX + c]
@@ -117,12 +128,20 @@ def main():
             print(f'bad file: {path}')
             exit(1)
 
-        train(ngrams[c], files[Use.train])
+        for u in Use:
+            with open(files[u], 'r', errors='replace') as f:
+                #if u == Use.validate:
+                #    sets[u] = f.read().split(' ')
+                #else:
+                sets[u] = f.read().splitlines()
 
-        with open(files[Use.validate], 'r', errors='replace') as f:
-            words = f.read().split(' ')
-        print(f'Perplexity: {perplexity(ngrams[c], words)}')
-        test(ngrams, files[Use.test], files[Use.label])
+        for l in range(N):
+            train(ngrams[l][c], sets[Use.train], l + 1)
+            prplxty = perplexity(ngrams[l][c], sets[Use.validate], l + 1, 0.1)
+            print(f'Class: {c.name}, Length: {l + 1}, Perplexity: {prplxty}')
+
+    for l in range(N):
+        test(ngrams[l], sets[Use.test], sets[Use.label], l + 1, 0.1)
 
 if __name__ == '__main__':
         main()
